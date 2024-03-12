d09286ea3c4c46aa43e966ab4db9e77dfc6d234d
Pedro Henrique Ferreira de Souza <pedro.souza@mondoin.com>
2024-03-12 16:07:27 -0400

    UseInterval hook and pomodoro timer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
 }
 
 export default App;
